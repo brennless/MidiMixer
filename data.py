781bd013cd1042603de897b18306162e53aa31b4
Alois Maier <alois.maier@bordeaux-inp.fr>
2025-11-11 23:16:36 +0100

    small changes

--- data.py
+++ data.py
@@ -16,34 +16,33 @@ def list_to_dict(l):
 
 def update_data():
     
-    pulse = pulsectl.Pulse('Updater')
+    with pulsectl.Pulse('Updater') as pulse:
+        sinks_raw = pulse.sink_list()
+        sink_inputs_raw = pulse.sink_input_list()
+        sources_raw = pulse.source_list()
+        source_outputs_raw = pulse.source_output_list()
+        cards_raw = pulse.card_list()
 
-    sinks_raw = pulse.sink_list()
-    sink_inputs_raw = pulse.sink_input_list()
-    sources_raw = pulse.source_list()
-    source_outputs_raw = pulse.source_output_list()
-    cards_raw = pulse.card_list()
+        midi_in = rtmidi.MidiIn()
+        in_ports_raw = midi_in.get_ports()
 
-    midi_in = rtmidi.MidiIn()
-    in_ports_raw = midi_in.get_ports()
+        sinks = list_to_dict(sinks_raw)
+        sources = list_to_dict(sources_raw)
+        sink_inputs = list_to_dict(sink_inputs_raw)
+        source_outputs = list_to_dict(source_outputs_raw)
+        in_ports = {}
+        for i, m in enumerate(in_ports_raw):
+            in_ports[m] = i
 
-    sinks = list_to_dict(sinks_raw)
-    sources = list_to_dict(sources_raw)
-    sink_inputs = list_to_dict(sink_inputs_raw)
-    source_outputs = list_to_dict(source_outputs_raw)
-    in_ports = {}
-    for i, m in enumerate(in_ports_raw):
-        in_ports[m] = i
+        data = {
+                "output_devices":sinks,
+                "input_devices":sources, 
+                "playback":sink_inputs,
+                "record":source_outputs,
+                "midi_in":in_ports
+                }
 
-    data = {
-            "output_devices":sinks,
-            "input_devices":sources, 
-            "playback":sink_inputs,
-            "record":source_outputs,
-            "midi_in":in_ports
-            }
-
-    json_data = json.dumps(data, indent=4)
-    print(json_data)
-    with open("data.json", "w") as f:
-        f.write(json_data)
+        json_data = json.dumps(data, indent=4)
+        print(json_data)
+        with open("data.json", "w") as f:
+            f.write(json_data)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# main.py
 import json
 import threading
 import queue
@@ -19,39 +20,51 @@ from data import update_data
 #
 CONFIG_PATH = Path.cwd().joinpath('config.json')
 
-
 midi_event_queue = queue.Queue()
 
 mapping = {}
 
-pulse = pulsectl.Pulse('MidiMixer')
 
-sinks_raw = pulse.sink_list()
+"""sinks_raw = pulse.sink_list()
 sink_inputs_raw = pulse.sink_input_list()
 sources_raw = pulse.source_list()
 source_outputs_raw = pulse.source_output_list()
-cards_raw = pulse.card_list()
-
-midi_in = rtmidi.MidiIn()
-in_ports_raw = midi_in.get_ports()
+cards_raw = pulse.card_list()"""
 
 ###############################################################################
 #
 # MIDI CALLBACK
 #
-def midi_callback(message, data):
+def midi_callback(message, device):
     """Will process midi messages"""
+    msg = (message, device)
+    midi_event_queue.put(msg)
+
+def midi_handler():
+    while True:
+        try:
+            msg = midi_event_queue.get(timeout=1)
+            status, cc, velocity = msg[0][0]
+            channel = status & 0x0F
+            command = status & 0xF0
+            print(f"Device:{msg[1]} ", end="")
+            if command == 176:
+                print(f"Knob {cc} to {velocity}")
+            elif command == 128:
+                print(f"Unmute {cc}")
+            elif command == 144:
+                print(f"Mute {cc}")
+        except queue.Empty:
+            continue
 
 
 ###############################################################################
 #
 # DISPATCHER
 #
-def dispatcher():
+def dispatcher(pulse):
     """Will coordinate everyone"""
 
-def handler():
-    """Will transmit data to pipewire"""
 
 
 ###############################################################################
@@ -70,14 +83,43 @@ class ConfigHandler(FileSystemEventHandler):
 def load_config():
     """Will load the config file"""
 
-
 ###############################################################################
 #
 # MAIN I GUESS
 #
 def main():
-    """Startup"""
+    """Startup""" 
     update_data()
+
+    config_handler = ConfigHandler(CONFIG_PATH)
+    observer = Observer()
+    observer.schedule(config_handler, CONFIG_PATH, recursive=False)
+    observer.start()
+
+    with pulsectl.Pulse('MidiMixer') as pulse:
+        
+        # Dispatcher
+        dispatcher_thread = threading.Thread(target=dispatcher, args=(pulse,), daemon=True)
+        dispatcher_thread.start()
+        
+
+        # Midi
+        midi_inputs = []
+        ports = rtmidi.MidiIn().get_ports()
+
+        for i, name in enumerate(ports):
+            midi_in = rtmidi.MidiIn()
+            midi_in.open_port(i)
+            midi_in.set_callback(midi_callback)
+            midi_inputs.append(midi_in)
+            print(f"[MIDI] Opened input: {name}") 
+        midi_in.set_callback(lambda message, data, device=name: midi_callback(message, device))
+        midi_thread = threading.Thread(target=midi_handler, daemon=True)
+        midi_thread.start()
+
+
+        while True:
+            """"""
 
 if __name__ == "__main__":
     main()
